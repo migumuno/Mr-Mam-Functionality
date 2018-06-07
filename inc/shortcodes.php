2c4a17068a4d51e6617067563d6737afd888acbc
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-07 10:21:17 +0200

    Añado shortcode para las formas de contacto

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -22,3 +22,21 @@ function mr_mam_print_blog_posts($atts) {
     endif;
 }
 add_shortcode( 'mr-mam-print-blog-posts', 'mr_mam_print_blog_posts' );
+
+/**
+ * Shortcode que imprime las formas de contacto
+ */
+function mr_mam_formas_contacto() {
+    $html = '
+    <ul class="formas-de-contacto">
+        <li class="welcome-icons_list"><a href="mailto:hey@mistermam.com"><i class="fas fa-envelope"></i></a></li>
+        <li class="welcome-icons_list"><a href="tel:+34696984784"><i class="fas fa-phone"></i></a></li>
+        <li class="welcome-icons_list"><a href="https://www.linkedin.com/in/miguelangelmunozviejo/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+        <li class="welcome-icons_list"><a href="https://twitter.com/mrmam_code" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li class="welcome-icons_list"><a href="<?=get_stylesheet_directory_uri()?>/docs/cv.pdf" download target="_blank"><i class="fas fa-file-alt"></i></a></li>
+    </ul>
+    ';
+
+    echo $html;
+}
+add_shortcode( 'formas_contacto', 'mr_mam_formas_contacto' );
