d11fae2207297dc73a004c9cdd4d7578d24b65b8
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-07 10:25:30 +0200

    Ajusto shorcodes

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -18,7 +18,7 @@ function mr_mam_print_blog_posts($atts) {
     $res = mr_mam_get_some_posts($atts['num'], $atts['type']);
 
     if( $res['success'] ):
-        echo $res['html'];
+        return $res['html'];
     endif;
 }
 add_shortcode( 'mr-mam-print-blog-posts', 'mr_mam_print_blog_posts' );
@@ -33,10 +33,10 @@ function mr_mam_formas_contacto() {
         <li class="welcome-icons_list"><a href="tel:+34696984784"><i class="fas fa-phone"></i></a></li>
         <li class="welcome-icons_list"><a href="https://www.linkedin.com/in/miguelangelmunozviejo/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
         <li class="welcome-icons_list"><a href="https://twitter.com/mrmam_code" target="_blank"><i class="fab fa-twitter"></i></a></li>
-        <li class="welcome-icons_list"><a href="<?=get_stylesheet_directory_uri()?>/docs/cv.pdf" download target="_blank"><i class="fas fa-file-alt"></i></a></li>
+        <li class="welcome-icons_list"><a href="'.get_stylesheet_directory_uri().'/docs/cv.pdf" download target="_blank"><i class="fas fa-file-alt"></i></a></li>
     </ul>
     ';
 
-    echo $html;
+    return $html;
 }
 add_shortcode( 'formas_contacto', 'mr_mam_formas_contacto' );
