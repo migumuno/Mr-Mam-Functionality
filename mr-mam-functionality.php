195a40022821040ff58245bc19241f83dfcd7a53
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-07 08:43:41 +0200

    Subida inicial con archivo base de plugin

---
+++ mr-mam-functionality.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Mr. Mam Functionality
+ * Plugin URI:      https://mistermam.com
+ * Description:     Plugin que alberga la funcionalidad de la web de mistermam.com
+ * Author:          Mr. Mam
+ * Author URI:      https://mistermam.com
+ * Text Domain:     mr-mam
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Mr_Mam
+ */
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
