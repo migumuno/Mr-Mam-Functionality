60a5e7a74fb107062a9339cf7f6da50a40cfe01a
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-18 19:30:39 +0200

    Actualizo la función que devuelve posts del blog añadiendo una capa a la imagen del post

--- inc/functions.php
+++ inc/functions.php
@@ -44,9 +44,14 @@ function mr_mam_get_some_posts($num = 4, $type = 'post') {
 
         while ( $my_query->have_posts() ) {
             $my_query->the_post();
+            $title = get_the_title();
+            if(strlen(get_the_title()) > 36) {
+                $title = substr($title, 0, 36).'...';
+            }
+
             $html .= '<div class="mr-mam-blog-posts__post '.$class.'">
-                '.get_the_post_thumbnail().'
-                <h3><a href="'.get_permalink().'">'.get_the_title().'</a></h3>
+                <div class="mr-mam-blog-posts__post--img">'.get_the_post_thumbnail().'</div>
+                <h3><a href="'.get_permalink().'">'.$title.'</a></h3>
             </div>';
         }
         
